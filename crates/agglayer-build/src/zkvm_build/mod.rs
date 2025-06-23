34f734d8df34e1b2d1a3f46dd1f88adac28362d4
L Kuklinek <lkuklinek@polygon.technology>
2025-06-23 17:23:50 +0100

    build: intorduce Agglayer ELF builder

---
+++ crates/agglayer-build/src/zkvm_build/mod.rs
@@ -0,0 +1,90 @@
+//! Building zkvm ELF binaries, Agglayer-style.
+//!
+//! This module provides a build script library that can be used to build
+//! zkvm ELF binaries. It is based on [sp1_build] but there is a couple
+//! differences to capture the build patterns used in Agglayer.
+//!
+//! * The docker-based deterministic build is always used.
+//! * The path to the zkvm program crate is specified relative to the host
+//!   workspace root.
+//! * The ELF binary is also emitted into the source tree so it can be checked
+//!   into the source tree.
+//! * By default, the checked in ELF is used so the project can be built without
+//!   the zkvm toolcahin.
+//!
+//! ## Controlling the build process using `AGGLAYER_BUILD`
+//!
+//! The build mode is specified using the `AGGLAYER_BUILD` environment variable.
+//!
+//! * Set it to 'build' to rebuild the program from source (requires docker).
+//! * Set it to 'refresh' to rebuild the program from source and update the
+//!   checked in binary (requires docker).
+//! * Set it to 'cached' to use the cached checked-in binary (default).
+//!
+//! After the build mode, the `AGGLAYER_BUILD` variable may contain extra
+//! arguments to be passed to the build. The accepted arguments are the same as
+//! what `cargo prove build` accepts.
+//!
+//! Example: `AGGLAYER_BUILD="refresh --warning-level=minimal" cargo build`.
+//!
+//! ## Building a zkvm ELF binary
+//!
+//! Decide which crate will expose the ELF binary.
+//!
+//! Inside that crate, use [build_program] and point it to the zkvm program
+//! crate. Note the path is relative to the workspace root:
+//!
+//! ```ignore
+//! fn main() {
+//!     agglayer_build::build_program("crates/whatever-proof-program").unwrap();
+//! }
+//! ```
+//!
+//! If more customization is required, [ProgramBuilder] may be used instead.
+//!
+//! In the crate source, include the resulting binary like this:
+//!
+//! ```ignore
+//! pub const ELF: &[u8] = include_bytes!(env!("AGGLAYER_ZKVM_ELF_PATH"));
+//! ```
+//!
+//! ## Caveats
+//!
+//! (1) Due to limitations of Rust build scripts, it is not possible to depend
+//! the cached zkvm ELF. Something like
+//! `include_bytes!("path/to/cached/elf/riscv32im-succinct-zkvm-elf")` is prone
+//! to build time race conditions. Instead, depend on the crate and use the
+//! `elf_crate::ELF` constant defined as suggested above.
+//!
+//! (2) By default, `cargo build` and similar commands do not emit output coming
+//! from build scipts. In this case, the build script builds the whole proof
+//! program including its dependencies. It may appear that the build is stuck
+//! for some time. Passing `-vv` to cargo makes it emit the build script output
+//! but it also makes the whole output much noisier.
+//!
+//! (3) The build script does not currently make any attempt to communicate the
+//! number of CPU cores taken up by currently running host build to the cargo
+//! command in the Docker container. This may result in too much concurrency.
+//!
+//! Note that (2) and (3) also apply to upstream [sp1_build].
+
+use cargo_metadata::camino::{Utf8Path, Utf8PathBuf};
+
+pub use self::{
+    config::{Config, Mode},
+    program_builder::ProgramBuilder,
+};
+
+mod config;
+mod program_builder;
+
+pub const DEFAULT_DOCKER_TAG: &str =
+    "v5.0.0@sha256:52d9e522d1dcbc4062edee950391173aed752793c33361fb2cad44272027a68c";
+
+/// Path to the cached zkvm ELF binary, relative to `build.rs`.
+pub const CACHED_ELF_PATH: &str = "elf/riscv32im-succinct-zkvm-elf";
+
+/// Convenience function to build the zkvm ELF if no customization is needed.
+pub fn build_program(program_dir: impl AsRef<Utf8Path>) -> anyhow::Result<Utf8PathBuf> {
+    ProgramBuilder::new(program_dir)?.run()
+}
---
+++ crates/agglayer-build/src/zkvm_build/program_builder.rs
@@ -0,0 +1,161 @@
+use std::{env, fs, path::Path};
+
+use anyhow::Context;
+use cargo_metadata::{
+    camino::{Utf8Path, Utf8PathBuf},
+    Metadata, MetadataCommand,
+};
+use sp1_build::BuildArgs;
+
+use super::{Config, Mode};
+
+pub struct ProgramBuilder {
+    /// Directory with the zkvm program source.
+    program_dir: Utf8PathBuf,
+
+    /// The path to the cached zkvm ELF binary.
+    cached_elf_path: Utf8PathBuf,
+
+    /// SP1 build arguments.
+    build_args: BuildArgs,
+
+    /// Zkvm program Cargo metadata.
+    program_metadata: Metadata,
+}
+
+impl ProgramBuilder {
+    /// New zkvm ELF builder.
+    pub fn new(program_dir: impl AsRef<Utf8Path>) -> anyhow::Result<Self> {
+        let host_metadata = MetadataCommand::new()
+            .no_deps()
+            .exec()
+            .context("Host workspace metadata extraction failed")?;
+
+        let program_dir = host_metadata.workspace_root.join(program_dir);
+
+        let cached_elf_path =
+            Utf8Path::new(env!("CARGO_MANIFEST_DIR")).join("elf/riscv32im-succinct-zkvm-elf");
+
+        let program_metadata = MetadataCommand::new()
+            .no_deps()
+            .current_dir(&program_dir)
+            .exec()
+            .context("Program workspace metadata extraction failed")?;
+
+        let build_args = BuildArgs {
+            docker: true,
+            tag: String::from(super::DEFAULT_DOCKER_TAG),
+            workspace_directory: Some(host_metadata.workspace_root.to_string()),
+            ..Default::default()
+        };
+
+        Ok(Self {
+            program_dir,
+            cached_elf_path,
+            build_args,
+            program_metadata,
+        })
+    }
+
+    /// Use the specified docker tag for the toolchain image instead of the
+    /// default one.
+    pub fn docker_tag(mut self, docker_tag: impl Into<String>) -> Self {
+        self.build_args.tag = docker_tag.into();
+        self
+    }
+
+    /// Specify the path where the cached zkvm ELF binary lives.
+    pub fn cached_elf_path(mut self, cached_elf_path: impl Into<Utf8PathBuf>) -> Self {
+        self.cached_elf_path = cached_elf_path.into();
+        self
+    }
+
+    /// Add build arguments. Same as passed to `cargo prove`.
+    pub fn add_args(mut self, extra_args: impl IntoIterator<Item = String>) -> Self {
+        clap::Parser::update_from(&mut self.build_args, extra_args);
+        self
+    }
+
+    /// Add argumets passed through to `rustc`.
+    pub fn add_rustflags(mut self, extra_args: impl IntoIterator<Item = String>) -> Self {
+        self.build_args.rustflags.extend(extra_args);
+        self
+    }
+
+    /// Get the path to the produced zkvm ELF in the build folder.
+    fn elf_path(&self) -> anyhow::Result<Utf8PathBuf> {
+        let args = &self.build_args;
+        let mut paths_iter = sp1_build::generate_elf_paths(&self.program_metadata, Some(args))
+            .context("Failed to extract zkvm ELF paths")?
+            .into_iter();
+
+        let (_package, path) = paths_iter.next().context("No zkvm ELF paths")?;
+        anyhow::ensure!(paths_iter.next().is_none(), "Too many zkvm ELF paths");
+
+        Ok(path)
+    }
+
+    fn copy_elf(source: impl AsRef<Path>, destination: impl AsRef<Path>) -> anyhow::Result<()> {
+        // Create all the necessary directories first
+        if let Some(cached_elf_dir) = destination.as_ref().parent() {
+            fs::create_dir_all(cached_elf_dir)
+                .context("Failed to create directory for zkvm ELF")?;
+        }
+
+        // Copy to a temporary and move so it's less sensitive to partial writes when
+        // the build is interrupted.
+        let source_tmp = source.as_ref().with_extension(".temp");
+        fs::copy(source, source_tmp.as_path()).context("Failed to copy zkvm ELF")?;
+        fs::rename(source_tmp, destination).context("Failed to move zkvm ELF")?;
+
+        Ok(())
+    }
+
+    fn build_program(self) -> anyhow::Result<Utf8PathBuf> {
+        let elf_path = self.elf_path()?;
+        sp1_build::build_program_with_args(self.program_dir.as_str(), self.build_args);
+        println!("cargo::rustc-env=AGGLAYER_ZKVM_ELF_PATH={elf_path}");
+
+        Ok(elf_path)
+    }
+
+    fn build_and_refresh(self) -> anyhow::Result<Utf8PathBuf> {
+        let cached_elf_path = self.cached_elf_path.clone();
+        let elf_path = self.elf_path()?;
+
+        self.build_program()?;
+        Self::copy_elf(elf_path.as_path(), cached_elf_path).context("Copying zkvm ELF to cache")?;
+
+        Ok(elf_path)
+    }
+
+    fn take_from_cache(self) -> anyhow::Result<Utf8PathBuf> {
+        let cached_elf_path = self.cached_elf_path.as_path();
+        let elf_path = self.elf_path()?;
+
+        println!("cargo::rerun-if-changed={cached_elf_path}");
+        println!("cargo::rustc-env=AGGLAYER_ZKVM_ELF_PATH={elf_path}");
+
+        Self::copy_elf(cached_elf_path, elf_path.as_path())
+            .context("Copying zkvm ELF from cache")?;
+
+        Ok(elf_path)
+    }
+
+    /// Run with given mode or config.
+    pub fn run_with(self, config: impl Into<Config>) -> anyhow::Result<Utf8PathBuf> {
+        let Config { mode, extra_args } = config.into();
+        let this = self.add_args(extra_args);
+
+        match mode {
+            Mode::Build => this.build_program(),
+            Mode::Refresh => this.build_and_refresh(),
+            Mode::Cached => this.take_from_cache(),
+        }
+    }
+
+    /// Run the zkvm ELF builder.
+    pub fn run(self) -> anyhow::Result<Utf8PathBuf> {
+        self.run_with(Config::from_env()?)
+    }
+}
